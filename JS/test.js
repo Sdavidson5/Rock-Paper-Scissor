539e5916305d399e98e39b12dc3e583ee0dffa9b
Sdavidson5 <86085106+Sdavidson5@users.noreply.github.com>
2024-10-06 12:50:54 -0400

    Added test files
    Added test files from Davidson Data

---
+++ JS/test.js
@@ -0,0 +1,22 @@
+/* This is basic js for notres adding button, will need rework when done
+const element = document.querySelector('button');
+console.log(element);
+
+const element2 = document.querySelector('.bee');
+element2.textContent = '9b done!';
+
+
+function nameInput() {
+    if(event.key === 'Enter') {
+        const inputElement = document.querySelector('.name');
+        const text= inputElement.value;
+        const nameElement = document.querySelector('.nameAnswer');
+        nameElement.textContent = `Your name is:${text}`
+    } else if( event ===''){
+    nameElement.textContent = ``;
+    }   
+}
+*/
+
+
+
